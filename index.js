b4c58c59a5e42025c0544215105cb53520d5660e
mosesbalaji <moses.balaji-external@tcs.com>
2022-01-07 12:10:05 +0800

    List repo

--- index.js
+++ index.js
@@ -14,33 +14,5 @@ async function run() {
     , maxRetries = getRequiredInput('octokit_max_retries')
   ;
   console.log(organizationinp)
-  let regex = /^[\w\.\_\-]+((,|-)[\w\.\_\-]+)*[\w\.\_\-]+$/g;
-  let validate_org = regex.test(organizationinp);
-  if((!validate_org)) {
-    throw new Error('Provide a valid organization - It accept only comma separated value');
-  }
 
-  let sinceregex = /^(20)\d\d-(0[1-9]|1[012])-([012]\d|3[01])T([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/ 
-;
-
-await io.mkdirP(outputDir)
-
-  const octokit = githubClient.create(token, maxRetries)
-    , orgActivity = new OrganizationActivity(octokit)
-  ;
-  
-  //***start */
-  let organizationlist = organizationinp.split(',');
-  let removeMulUserList = [];
-  let repos = [];
-  let rmvconfrm = 0;
-  console.log(organizationlist)
-  for(const organization of organizationlist){
-    console.log(`Attempting to generate ${organization} - user activity data, this could take some time...`);
-    const orgsComments = await orgActivity.getOrgsValid(organization);
-    if(orgsComments.status !== 'error') {
-         repos = await orgActivity.listForOrg(organization);
-    }
-  }
-  core.setOutput('repos', repos);
 }
